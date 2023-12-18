f778c1f8716496901e69ed9835b6a69e80101b7e
jhlee <jhlee@rianag.com>
2023-12-18 11:23:35 +0900

    인덱스 시작일종료일 추가

--- js/guide.js
+++ js/guide.js
@@ -2,58 +2,70 @@ $(document).ready(function(){
     var page_arry = [
         {
             depth:['모달','간편비밃너호 설정 완료','',''],
-            id:'app-password-002-1', status:'',
+            id:'app-password-002-1', status:'com',
             type:'modal', note:'모달',
+            sDate:'', eDate:'2023-12-15'
         },{
             depth:['','통신사 선택','',''],
-            id:'app-signin-001-1', status:'',
+            id:'app-signin-001-1', status:'com',
             type:'modal', note:'모달',
+            sDate:'', eDate:'2023-12-15'
         },{
             depth:['','약관 동의','',''],
-            id:'app-signin-002', status:'',
+            id:'app-signin-002', status:'com',
             type:'modal', note:'모달',
+            sDate:'', eDate:'2023-12-15'
         },{
             depth:['','인증번호 입력','',''],
-            id:'app-signin-003', status:'',
+            id:'app-signin-003', status:'com',
             type:'modal', note:'모달',
+            sDate:'', eDate:'2023-12-15'
         },
 
         {
             depth:['회원가입','본인인증','',''],
             id:'', status:'',
             note:'',
+            sDate:'', eDate:''
         },
         {
             depth:['','휴대폰본인인증','',''],
             id:'', status:'',
             note:'',
+            sDate:'', eDate:''
         },{
             depth:['','간편비밀번호설정','',''],
             id:'', status:'',
             note:'',
+            sDate:'', eDate:''
         },{
             depth:['','생체인증사용설정','',''],
             id:'', status:'',
             note:'',
+            sDate:'', eDate:''
         },
         {
             depth:['','DID발급/가입완료','',''],
             id:'', status:'',
             note:'',
+            sDate:'', eDate:''
         },
 
         {
             depth:['로그인','간편비밀번호','',''],
             id:'app-login-001', status:'com',
             note:'',
+            sDate:'', eDate:'2023-12-15'
         },{
             depth:['','본인확인','',''],
             id:'app-password-001', status:'com',
             note:'약관 동의[modal > app-signin-002], 인증번호 입력[app-signin-003]',
+            sDate:'', eDate:'2023-12-15'
         },{
             depth:['','간편비밀번호','',''],
             id:'app-password-002', status:'com',
             note:'[app-login-001] 로그인-간편비밀번호 타이틀 교체 사용, [modal > app-password-002-1] 포함',
+            sDate:'', eDate:'2023-12-15'
         }
     ];
 
@@ -82,6 +94,8 @@ $(document).ready(function(){
         +   '<td class="'+page_arry[i].status+'">'+page_arry[i].depth[3]+'</td>'
         +   '<td class="ac '+page_arry[i].status+'"><a href="./'+path+'/'+page_arry[i].id+'.html" target="_blank">'+page_arry[i].id+'</a></td>'
         +   '<td class="'+page_arry[i].status+'">'+page_arry[i].note+'</td>'
+        +   '<td class="ac '+page_arry[i].status+'">'+page_arry[i].sDate+'</td>'
+        +   '<td class="ac '+page_arry[i].status+'">'+page_arry[i].eDate+'</td>'
         +'</tr>');
     }
 });
