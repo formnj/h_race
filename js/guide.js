a5bf11adb5a07558efdd1934305095aff3007115
jhlee <jhlee@rianag.com>
2023-12-19 12:23:42 +0900

    add list

--- js/guide.js
+++ js/guide.js
@@ -69,7 +69,105 @@ $(document).ready(function(){
             id:'app-password-002', status:'com',
             note:'[app-login-001] 로그인-간편비밀번호 타이틀 교체 사용, [modal > app-password-002-1] 포함',
             sDate:'', eDate:'2023-12-15'
+        },
+        
+        {
+            depth:['메인','','',''],
+            id:'app-main-001', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },
+        
+        {
+            depth:['계좌관리','증권계좌개설','증권계좌 개설안내',''],
+            id:'app-account-001', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','계좌보유여부확인','계좌보유'],
+            id:'app-account-002', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','계좌미보유'],
+            id:'app-account-003', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','계좌개설 시작','개설시 약관동의'],
+            id:'app-account-004', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','기본정보입력'],
+            id:'app-account-005', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','신분증촬영'],
+            id:'app-account-006', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','직업정보입력/거래정보입력'],
+            id:'app-account-007', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','계좌비밀번호등록(4자리)'],
+            id:'app-account-008', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','입금졔좌연결'],
+            id:'app-account-009', status:'',
+            note:'연결계좌입력 / 1원송금확인',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','계좌개설 완료화면'],
+            id:'app-account-010', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },
+
+        {
+            depth:['투자성향','투자성향 조사','투자성향 설문안내',''],
+            id:'app-survay-001', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','투자성향 설문시작','설문1)투자희망/정보제공'],
+            id:'app-survay-002', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','설문2)설문항목답변(9가지)'],
+            id:'app-survay-003', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','투자성향 제출하기','투자성향 설문 확인서 확인/제출'],
+            id:'app-survay-004', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','확인완료 내역 본인확인(간편비밀번호or지문인증)'],
+            id:'app-survay-005', status:'',
+            note:'',
+            sDate:'', eDate:''
+        },{
+            depth:['','','','투자성상품거래확인서(설문결과서)'],
+            id:'app-survay-006', status:'',
+            note:'',
+            sDate:'', eDate:''
         }
+        
+        // {
+        //     depth:['','','',''],
+        //     id:'', status:'',
+        //     note:'',
+        //     sDate:'', eDate:''
+        // }
     ];
 
     var row_chk,
